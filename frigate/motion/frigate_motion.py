1a07b85511ecfb718bd9214cbba4e138315d56ff
skdash70007 <skdash70007@gmail.com>
2025-08-28 23:28:23 +0530

    pr: Update frigate_motion.py
    added comment

--- frigate/motion/frigate_motion.py
+++ frigate/motion/frigate_motion.py
@@ -70,7 +70,7 @@ class FrigateMotionDetector(MotionDetector):
         # it takes ~30 frames to establish a baseline
         # dont bother looking for motion
         if self.frame_counter < 30:
-            print("hi")
+            print("hi") # Required for debugging
             self.frame_counter += 1
         else:
             if self.save_images:
